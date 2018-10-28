a494b2cff3df2094309380f6cae41d9bae2eb138
ArtemSkok <temaskok98@gmail.com>
2018-10-28 17:28:13 +0200

    saving image from ndarray

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const getPixels = require('get-pixels');
+const savePixels = require('save-pixels');
 const ndarray = require('ndarray');
+const fs = require('fs');
 
 let time;
 const startTime = new Date();
@@ -8,19 +10,28 @@ getPixels('./public/src-images/distorted.png', 'image/png', (err, pixels) => {
     if (err) {
         console.error(err);
     } else {
-        const processedPixelsArr = ndarray(new Uint8Array(pixels.size).fill(0));
+        const processedPixelsArr = ndarray(
+            new Uint8Array(pixels.size).fill(0),
+            pixels.shape,
+            pixels.stride,
+            pixels.offset
+        );
 
         const matrixY = pixels.shape[0],
             matrixX = pixels.shape[1],
-            matrixZ = pixels.shape[2];
+            channels = pixels.shape[2];
 
         for (let y = 0; y < matrixY; y++) {
             for (let x = 0; x < matrixX; x++) {
-                for (let z = 0; z < matrixZ; z++) {
+                for (let z = 0; z < channels; z++) {
                     processedPixelsArr.set(y, x, z, pixels.get(y, x, z));
                 }
             }
         }
+        const buffer = fs.createWriteStream(
+            './public/processed-images/res.png'
+        );
+        savePixels(processedPixelsArr, 'png').pipe(buffer);
 
         time = new Date() - startTime;
         console.log(`ms spend: ${time};`);
